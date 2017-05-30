0d6e1006a0e0b4b3a7eb212034fe7d2e86028136
amohammed <amohammed@logicworks.net>
2017-05-30 09:39:23 -0400

    still handing clean exit

--- nacl.py
+++ nacl.py
@@ -7,12 +7,14 @@ ec2 = boto3.client('ec2')
 
 def main():
     target_ip = raw_input('Enter Target IP: ')
-    while False:
+    while True:
         if target_ip not in get_network_interfaces_info().keys():
             print "The IP you entered is nowhere to be found. Please try again below."
-    else:
-        print '--------------------------------------'
-        print "Nacl to block Source IP: {0}".format(nacl_of(target_ip))
+        else:
+            print '--------------------------------------'
+            print "Nacl to block Source IP: {0}".format(nacl_of(target_ip))
+
+
 
 def nacl_of(target_ip):
     subnet = get_network_interfaces_info()[target_ip]
@@ -28,12 +30,11 @@ def get_network_interfaces_info():
 def get_network_acls():
     nacl = ec2.describe_network_acls(Filters=[{'Name':'association.subnet-id', 'Values':['*']}])['NetworkAcls']
     return  {item['NetworkAclId']:[subnet['SubnetId'] for subnet in item['Associations']] for item in nacl}
-def shutting_down():
-    print "Shutdown requested...exiting"
-    sys.exit(130)
+
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        shutting_down()
+        print "Shutdown requested...exiting"
+        sys.exit(130)
